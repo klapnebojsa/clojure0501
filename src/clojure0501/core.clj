d77d0de0501156f52df6a35930417d04de7bb866
klapnebojsa <klapnebojsa@gmail.com>
2015-12-20 02:00:29 +0100

    .

--- src/clojure0501/core.clj
+++ src/clojure0501/core.clj
@@ -1,4 +1,5 @@
-(ns clojure0501.core)
+(ns clojure0501.core
+  (:use clojure.pprint))
 
 ; M A C R O
 (defmacro foreach [[sym coll] & body]
@@ -46,20 +47,129 @@
 `(       x             ~x            lst             ~@lst     7 8 :nine)
 ;(clojure0501.core/x    5    clojure0501.core/lst    a b c     7 8 :nine)
 
+(defmacro print-keyword [x]
+  `(println (keyword ~x)))
 
+;(print-keyword "foo")
+;:foo
 
+(require '(clojure [string :as str]
+                   [walk :as walk]))
+(defmacro reverse-it
+  [form]
+  (walk/postwalk #(if (symbol? %)
+                    (symbol (str/reverse (name %)))
+                    %)
+                 form))
+#_(reverse-it
+   (qesod [gra (egnar 5)]
+          (nltnirp (cni gra))))
+;1
+;2
+;3
+;4
+;5
 
+(defmacro oops [arg] `(frobnicate ~arg))
+;(oops 123)
+;CompilerException java.lang.RuntimeException: No such var: clojure0501.core/frobnicate, compiling:(clojure0501\core.clj:73:1)
+(macroexpand-1 '(oops 123))
+;(clojure0501.core/frobnicate 123)
 
+#_(macroexpand-1 '(reverse-it
+                     (qesod [gra (egnar 5)]
+                            (nltnirp (cni gra)))))
+;(doseq [arg (range 5)] (println (inc arg)))
 
+#_(pprint (macroexpand '(reverse-it
+                         (qesod [gra (egnar 5)]
+                                (nltnirp (cni gra))))))
 
+#_(loop*
+ [seq_11992
+  (clojure.core/seq (range 5))
+  chunk_11993
+  nil
+  count_11994
+  0
+  i_11995
+  0]
+ (if
+  (clojure.core/< i_11995 count_11994)
+  (clojure.core/let
+   [arg (.nth chunk_11993 i_11995)]
+   (do (println (inc arg)))
+   (recur
+    seq_11992
+    chunk_11993
+    count_11994
+    (clojure.core/unchecked-inc i_11995)))
+  (clojure.core/when-let
+   [seq_11992 (clojure.core/seq seq_11992)]
+   (if
+    (clojure.core/chunked-seq? seq_11992)
+    (clojure.core/let
+     [c__4356__auto__ (clojure.core/chunk-first seq_11992)]
+     (recur
+      (clojure.core/chunk-rest seq_11992)
+      c__4356__auto__
+      (clojure.core/int (clojure.core/count c__4356__auto__))
+      (clojure.core/int 0)))
+    (clojure.core/let
+     [arg (clojure.core/first seq_11992)]
+     (do (println (inc arg)))
+     (recur (clojure.core/next seq_11992) nil 0 0))))))
 
+(macroexpand '(cond a b c d))
+;(if a b (clojure.core/cond c d))
 
+(require '[clojure.walk :as w])
+;(w/macroexpand-all '(cond a b c d))
+;(if a b (if c d nil))
 
+;(w/macroexpand-all ''(when x a))
+;(quote (if x (do a)))
 
+(defmacro hello
+  [name]
+  (list 'println name))
+(macroexpand '(hello "Brian"))
+;(println "Brian")
+;(hello "Brian")
+;Brian
 
+(defmacro while1
+  [test & body]
+  (list 'loop []
+        (concat (list 'when test) body)
+        '(recur)))
+;PRAVA SINTAKSA ZA MAKRO-e
+(defmacro while1
+  [test & body]
+  `(loop []
+     (when ~test
+       ~@body
+       (recur))))
 
+(def foo 123)
+ [foo (quote foo) 'foo           `foo]
+;[123    foo       foo    clojure0501.core/foo]
 
+(list `map `println [foo])
+;(clojure.core/map clojure.core/println [123])
 
+`(map println [~foo])                                   ;Pametnije
+;(clojure.core/map clojure.core/println [123])
+`(map println ~[foo])
+;(clojure.core/map clojure.core/println [123])
+
+`(println ~(keyword (str foo)))
+;(clojure.core/println :123)
+
+(let [defs '((def x 123)       ; 'quote - string
+              (def y 456))]
+  (concat (list 'do) defs))     ;concat spaja u listu string do i vrednost u stringu defs=(def x 123) (def y 456)
+;(do (def x 123) (def y 456))
 
 
 
